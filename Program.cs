aacf26808687a740ce36770f35dfd949eb3b4b47
Randitya Hundal <67023582+RandityaHundal@users.noreply.github.com>
2023-11-10 21:37:25 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//main code
+
+
+Multiply(3,8);
+
+
+// close with key
+Console.ReadKey();
+
+// multiply function
+
+static void Multiply(int num1, int num2)
+{
+    int result = num1 * num2;
+    Console.WriteLine($"The result is {result}");
+}
